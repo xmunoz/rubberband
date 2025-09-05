c843c26afbb9d36a05dc2dda4a7d0b9736d1978b
Cristina Muñoz <cristina@cradle.app>
2025-09-05 11:16:35 -0500

    Fix homepage testrun loading logic

--- rubberband/utils/es_helpers.py
+++ rubberband/utils/es_helpers.py
@@ -17,17 +17,18 @@ def get_uniques(model, field):
     -------
         all possible values and the 5 most common ones.
     """
-    body = {'aggs': {'counts': {'terms': {'field': field, 'size': 0}}}}
+    body = {'aggs': {'counts': {'terms': {'field': field}}}}
     response = getattr(model, "search")().from_dict(body).execute()
     values = [i.key for i in response.aggregations.counts.buckets]
-
     today = datetime.now()
     threemonthsago = (today + timedelta(days=-100)).strftime('%Y-%m-%d')
-    # TODO make this work for the most recent testruns
-    body = {"filter": {"range": {"git_commit_timestamp": {"gte": threemonthsago}}},
-            "aggs": {"hot_counts": {"terms": {"field": field, "size": 5}}}}
+    body = {"query": {"bool": {"filter": {"range": {"git_commit_timestamp":
+        {"gte": threemonthsago}}}}},
+        "aggs": {"hot_counts": {"terms": {"field": field}}}}
 
-    response = getattr(model, "search")().from_dict(body).execute()
-    hot_values = [i.key for i in response.aggregations.hot_counts.buckets]
+    response = getattr(model, "search")().from_dict(body).execute()[:5]
+    hot_values = []
+    if response:
+        hot_values = [i.key for i in response.aggregations.hot_counts.buckets]
     values = [v for v in values if v not in hot_values]
     return values, hot_values
